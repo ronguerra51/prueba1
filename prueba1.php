738da59fdfcc6496de6b3e19156dae77957d0338
Roger <ronguerra51@gmail.com>
2024-04-29 09:04:55 -0600

    cambio

--- prueba1.php
+++ prueba1.php
@@ -1,3 +1,15 @@
 <?php
 echo "Hola mundo!!";
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>PROGRAMACION</h1>
+    <H2>INICIO</H2>
+</body>
+</html>
